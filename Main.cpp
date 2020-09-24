84f3ae0f0a28dd00e6a75bf17d35384e399a0f39
Andriy <a.palamat@obolon365.net>
2020-09-24 17:14:57 +0300

    Include mult ti main

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include"Summ.h"
 #include"Dif.h"
+#include"Mult.h"
 
 using namespace std;
 
@@ -10,6 +11,7 @@ int main(){
 	cout<<"Enter b -> ";cin>>b;
 	Summ(a,b);
 	Dif(a,b);
+	Mult(a,b);
 	system("Pause");
 }
 
